bc782056988e5813e2d41b6a8f4e866d5a25d734
biyaniyash23 <yash.biyani23@gmail.com>
2022-08-21 04:15:38 +0530

    moved trees from suprior to evo

--- device.mk
+++ device.mk
@@ -45,7 +45,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-superior
+    $(DEVICE_PATH)/overlay-evolution
 
 
 # Powerhint
