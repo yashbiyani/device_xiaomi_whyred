e58907b8c799b6f044efba23c9e389d87fe4d5ae
biyaniyash23 <yash.biyani23@gmail.com>
2022-08-26 23:21:16 +0000

    Whyred: Enable freeform window management

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ BOARD_HAVE_IR := true
 # FM
 BOARD_HAVE_QCOM_FM := true
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
