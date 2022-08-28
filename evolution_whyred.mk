ea4fbff7d923acef7f2a412520f91c5c7a30e4c2
biyaniyash23 <yash.biyani23@gmail.com>
2022-08-28 20:27:23 +0000

    Whyred: Update Build fp from Coral August'22 A13

--- evolution_whyred.mk
+++ evolution_whyred.mk
@@ -25,9 +25,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := whyred
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="whyred-user 8.1.0 OPM1.171019.011 V9.5.11.0.OEIMIFA release-keys"
+     PRIVATE_BUILD_DESC="google/coral/coral:13/TP1A.220624.014/8819323:user/release-keys"
 
-BUILD_FINGERPRINT := xiaomi/whyred/whyred:8.1.0/OPM1.171019.011/V9.5.11.0.OEIMIFA:user/release-keys
+# Build fingerprint
+BUILD_FINGERPRINT := "google/coral/coral:13/TP1A.220624.014/8819323:user/release-keys"
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_BOOTANIMATION_PRELOAD := true
