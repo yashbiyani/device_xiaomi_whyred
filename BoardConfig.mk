5ac284fdf97b195b6b8137243ea062b7d730d313
biyaniyash23 <yash.biyani23@gmail.com>
2022-11-02 19:16:01 +0000

    Whyred:Changed kernel name

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,7 @@ TARGET_SCREEN_DENSITY := 420
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/xiaomi/whyred
-TARGET_KERNEL_CONFIG := whyred-oldcam_defconfig
+TARGET_KERNEL_CONFIG := whyred_defconfig
 TARGET_KERNEL_CLANG_VERSION := proton
 
 # Manifest
